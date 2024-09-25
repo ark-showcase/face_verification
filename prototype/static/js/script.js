822af783fe19061edb636c1117d849f47d7f7f7f
ark-showcase <154075@student.duet.ac.bd>
2024-09-26 01:24:57 +0600

    face-api invoked

--- prototype/static/js/script.js
+++ prototype/static/js/script.js
@@ -1,1 +1,31 @@
-console.log('js connected');
+const video = document.getElementById('video')
+console.log(modelPath);
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri(modelPath),
+  faceapi.nets.faceLandmark68Net.loadFromUri(modelPath),
+  faceapi.nets.faceRecognitionNet.loadFromUri(modelPath),
+  faceapi.nets.faceExpressionNet.loadFromUri(modelPath)
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
